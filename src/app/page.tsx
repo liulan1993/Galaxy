a55b76445130152576d806cfc15ce4eb15a669e7
liulan1993 <plj453369670@gmail.com>
2025-06-28 08:55:11 +0800

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,7 +273,7 @@ function RadialOrbitalTimeline() {
           ref={orbitRef} 
           style={{ 
             perspective: "1000px", 
-            transform: 'translateX(42vw) translateY(40vh)' 
+            transform: 'translateX(40vw) translateY(38vh)' 
           }}
         >
           <div
